6abb9ebc7d46715b513c5edabe0b5fd37ffaedb2
poongodi1010 <137901761+poongodi1010@users.noreply.github.com>
2023-08-10 15:11:30 +0530

    Update game.js

--- game.js
+++ game.js
@@ -8,27 +8,32 @@ document.getElementById("btn-submit").addEventListener('click', function(event) 
 
 
   var nameValue = document.getElementById("name").value;
-  var emailValue = document.getElementById("mail").value;
+  var partnerValue = document.getElementById("mail").value;
+    var letters = /^[A-Za-z]+$/;
 
-  if ((nameValue.length === 0) && (emailValue.length === 0) ){
+  if ((nameValue.length === 0) && (partnerValue.length === 0) ){
     document.getElementById("youcheck").style.display = "block";
     document.getElementById("partnercheck").style.display = "block";
     started = false;
   }
   else
-   if ((nameValue.length !== 0) && (emailValue.length === 0) ) {
+   if ((nameValue.length !== 0) && (partnerValue.length === 0) ) {
     document.getElementById("youcheck").style.display = "none";
     document.getElementById("partnercheck").style.display = "block";
     started = false;
   }
 else
-  if ((nameValue.length === 0) && (emailValue.length !== 0) ) {
+  if ((nameValue.length === 0) && (partnerValue.length !== 0) ) {
     document.getElementById("youcheck").style.display = "block";
     document.getElementById("partnercheck").style.display = "none";
     started = false;
 
   }
-  else {
+  else if ((!nameValue.match(letters)) && (!partnerValue.match(letters))) {
+      document.getElementById("youcheck").innerHTML = "Enter Alphabet characters only..!!!";
+      document.getElementById("partnercheck").innerHTML = "Enter Alphabet characters only..!!!";
+  } 
+    else {
     started = true;
       percent();
   }
@@ -46,14 +51,12 @@ function percent() {
     if (loveNum >= 50) {
       var loveMsg = document.getElementById("forum");
       loveMsg.innerHTML = `💕Your love percentage is ${loveNum}%  🤗
-      You both are like 
-      Tom and Jerry even though you fight many times you wont be apart😍
+      You both are like Tom and Jerry even though you fight many times you wont be apart😍
       Cheers..!!!❤❤❤`;
 
     } else if (loveNum <= 50) {
       document.getElementById("forum").innerHTML = `❣Your love percentage is ${loveNum}% 🤗 
-      You both are like 
-      bread and Jam and you both are inseparable🥰
+      You both are like bread and Jam and you both are inseparable🥰
       Cheers..!!!❤❤❤`;
     }
   }
